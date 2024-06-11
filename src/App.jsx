eef6c5959c344cd618500fd4ae820fbbf83a05c6
kasimugur <kasiugur7@gmail.com>
2024-06-11 22:50:02 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { CoinContainer } from './container'
+// import RandomCoin from './container/RandomCoin'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <CoinContainer />
+    {/* <RandomCoin /> */}
     </>
   )
 }
--- src/container/CoinContainer.jsx
+++ src/container/CoinContainer.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react"
+
 export default function CoinContainer() {
+  const [tails, setTails] = useState(0)
+  const [heads, setHeads] = useState(0)
+  const [flips, setFlips] = useState(0)
+  const coin = {
+    coins: [
+      {
+        side: "tails",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/commons/6/64/1TL_obverse.png"
+      },
+      {
+        side: "heads",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/commons/c/cd/1TL_reverse.png"
+      }
+    ]
+  }
+  function handleClick() {
+    const newCoin = choice(coin.coins)
+    return {
+      currCoin: newCoin,
+      flips: setFlips(flips + 1),
+      heads: setHeads(newCoin.side === "tails" ? heads + 1 : heads),
+      tails: setTails(newCoin.side === "heads" ? tails + 1 : tails),
+    }
+  }
+
+  function choice(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length)
+    return arr[randomIndex]
+  }
   return (
     <>
       <div className="coinContainer">
         <h1>coin toos game</h1>
+        <h2>{flips} : flips </h2>
+        <h2>{tails} : tails </h2>
+        <h2>{heads} : heads</h2>
+        <button onClick={handleClick} className="btn">Flip</button>
       </div>
     </>
   )
